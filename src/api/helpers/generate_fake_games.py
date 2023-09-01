272af4b153eab86f7edadc527c997579cd27dc99
freakynoblegas <nickquinn3131@gmail.com>
2023-08-31 21:36:00 -0400

    Added endpoint to return all games and a model for Game, Multiplayer, and Platforms.

---
+++ src/api/helpers/generate_fake_games.py
@@ -0,0 +1,27 @@
+#
+# Description: Uses the Faker library to generate a JSON file with fake game data using the Game and Platform models.
+#
+from faker import Faker
+import random
+import json
+
+from src.api.models import platforms, multiplayer, game
+
+#from src.api.models import game, platforms, multiplayer
+
+NUM_OF_GAMES_TO_CREATE = 100
+
+fake = Faker()
+data = []
+for i in range(NUM_OF_GAMES_TO_CREATE):
+    title = ' '.join(fake.words(nb=random.randint(1, 3)))
+    description = fake.paragraph()
+    release_date = str(fake.date())
+    available_platforms = platforms.Platforms()
+    available_multiplayer = multiplayer.Multiplayer(random.randint(1, 20))
+    g = game.Game(title, description, available_platforms, release_date, available_multiplayer)
+
+    data.append(g.to_json())
+
+with open('../../../data/fake_games_dataset.json', 'w+') as outfile:
+    json.dump(data, outfile, indent=1)
---
+++ src/api/models/multiplayer.py
@@ -0,0 +1,22 @@
+class Multiplayer:
+    """
+    Args:
+        player_count (int, optional): Number of players available online.
+    Example:
+        multiplayer = Multiplayer(player_count=10)
+    """
+    def __init__(self, player_count=1):
+        self._player_count = player_count
+
+    @property
+    def player_count(self):
+        return self._player_count
+
+    @player_count.setter
+    def player_count(self, value):
+        self._player_count = value
+
+    def to_json(self):
+        return {
+            "player_count": self._player_count
+        }
