9e41ec25c61a8ec3e5d6118735501a2210c7e20c
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-08-31 20:45:51 -0500

    removed boilerplate, added basic footer

---
+++ src/app/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer class="absolute bottom-0 left-0 flex flex-row justify-between w-full px-5">
+      <h6 class="text-xs">Copyright @ 2023 Clever Penguin Design</h6>
+      <p class="text-xs">Contact Us</p>
+    </footer>
+  );
+};
+
+export default Footer;
