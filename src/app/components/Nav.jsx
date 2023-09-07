a68d6796c6e36623ef45e68124b08b54b63aaa44
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-09-07 18:32:44 -0500

    convert buttons to ul

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -15,41 +15,41 @@ const Nav = () => {
       <nav className="h-screen w-60 pt-16 pb-0 flex-col bg-secondary border-r border-black shadow-black">
         <Image src={logo} alt="logo" className="pl-7" />
 
-        <div className="w-48 mt-12 mb-96 pl-7">
-          <button className=" hover:bg-primary rounded-lg h-12 w-48 items-center flex space-x-4 pl-6">
+        <ul className="w-48 mt-12 mb-96 pl-7">
+          <li className=" hover:bg-primary rounded-lg h-12 w-48 items-center flex space-x-4 pl-6">
             <FontAwesomeIcon
               icon={faHouse}
               style={{ color: "#ffffff" }}
               fixedWidth
             />
-            <p className="">Home</p>
-          </button>
-          <button className=" hover:bg-primary rounded-lg h-12 w-48 items-center flex space-x-4 pl-6">
+            <p>Home</p>
+          </li>
+          <li className=" hover:bg-primary rounded-lg h-12 w-48 items-center flex space-x-4 pl-6">
             <FontAwesomeIcon
               icon={faMagnifyingGlass}
               style={{ color: "#ffffff" }}
               fixedWidth
             />
             <p className="">Search</p>
-          </button>
-          <button className="hover:bg-primary rounded-lg h-12 w-48 items-center flex space-x-4 pl-6">
+          </li>
+          <li className=" hover:bg-primary rounded-lg h-12 w-48 items-center flex space-x-4 pl-6">
             <FontAwesomeIcon
               icon={faGamepad}
               style={{ color: "#ffffff" }}
               fixedWidth
             />
-
             <p className="">My Games</p>
-          </button>
-          <button className=" hover:bg-primary rounded-lg h-12 w-48 items-center flex space-x-4 pl-6">
+          </li>
+          <li className=" hover:bg-primary rounded-lg h-12 w-48 items-center flex space-x-4 pl-6">
             <FontAwesomeIcon
               icon={faPen}
               style={{ color: "#ffffff" }}
               fixedWidth
             />
             <p className="">Wishlist</p>
-          </button>
-        </div>
+          </li>
+        </ul>
+
         <Avatar />
       </nav>
     </>
