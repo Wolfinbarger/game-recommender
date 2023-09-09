abdcf504d808a53331a88e154874a05e4c25d8bb
freakynoblegas <nickquinn3131@gmail.com>
2023-09-09 12:06:59 -0400

    Issue 16: Replaced FastAPI with Django and migrated our existing models.

--- src/api/api/settings.py
+++ src/api/api/settings.py
@@ -11,16 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d#()i59pwzsb)8q7+5az3e!oncmppj763y3z%nk+lccs=crb6m'
+SECRET_KEY = environ["SECURITY_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
