f9e6991f5cf71052e2fa940f01f77c5012382cee
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-09-10 10:24:09 -0500

    added space between text

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="flex flex-row px-5">
+    <footer className="flex flex-row justify-between px-5">
       <p className="text-xs">Copyright @ 2023 Clever Penguin Design</p>
-      <p className="text-xs">Contact Us</p>
+      <p className="text-xs ">Contact Us</p>
     </footer>
   );
 };
