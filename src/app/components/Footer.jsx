705fd045a753609bd809054bb25646e73833d35a
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-09-10 10:20:23 -0500

    update class for footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer class="flex flex-row px-5">
-      <p class="text-xs">Copyright @ 2023 Clever Penguin Design</p>
-      <p class="text-xs">Contact Us</p>
+    <footer className="flex flex-row px-5">
+      <p className="text-xs">Copyright @ 2023 Clever Penguin Design</p>
+      <p className="text-xs">Contact Us</p>
     </footer>
   );
 };
