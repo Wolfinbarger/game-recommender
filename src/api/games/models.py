d04b4c2624c8780f7e0f904484755db2c34b6db2
freakynoblegas <nickquinn3131@gmail.com>
2023-11-04 19:25:15 -0400

    Added model for IGDB game api endpoint.

--- src/api/games/models.py
+++ src/api/games/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 def to_json(items):
@@ -114,6 +115,109 @@ class Game(models.Model):
     # Foreign Keys
     multiplayer = models.ForeignKey(Multiplayer, on_delete=models.CASCADE, db_column='game_multiplayer')
     platform = models.ForeignKey(Platform, on_delete=models.CASCADE, db_column='game_platform')
+
+    def __str__(self):
+        return self.to_json().__str__()
+
+    def to_json(self):
+        return to_json(self.__dict__.items())
+
+
+class IGDBGame(models.Model):
+    # The IGDB id of the game
+    id = models.IntegerField(primary_key=True)
+    # The aggregated rating of the game
+    aggregated_rating = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The amount of ratings the game has received
+    aggregated_rating_count = models.IntegerField(null=True, blank=True)
+    # The alternative names of the game
+    alternative_names = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The artworks associated with the game
+    artworks = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The bundles that include the game
+    bundles = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The category of the game
+    category = models.IntegerField(null=True, blank=True)
+    # The checksum of the game
+    checksum = models.UUIDField(null=True, blank=True)
+    # The collection that the game belongs to
+    collection = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The cover image of the game
+    cover = models.IntegerField(null=True, blank=True)
+    # The creation date of the game
+    created_at = models.IntegerField(null=True, blank=True)
+    # The DLCs and expansions of the game
+    dlcs = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The external games linked to the game
+    external_games = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The first release date of the game
+    first_release_date = models.IntegerField(null=True, blank=True)
+    # The follows count of the game
+    follows = models.IntegerField(null=True, blank=True)
+    # The franchises that the game belongs to
+    franchises = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The game engines used by the game
+    game_engines = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The game modes available in the game
+    game_modes = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The genres of the game
+    genres = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The Hypescore of the game
+    hypes = models.IntegerField(null=True, blank=True)
+    # The involved companies in the game
+    involved_companies = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The keywords associated with the game
+    keywords = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The multiplayer modes available in the game
+    multiplayer_modes = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The name of the game
+    name = models.CharField(max_length=255, null=True, blank=True)
+    # The parent game of the game
+    parent_game = models.IntegerField(null=True, blank=True)
+    # The platforms that the game is released on
+    platforms = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The player perspectives of the game
+    player_perspectives = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The rating of the game
+    rating = models.FloatField(null=True, blank=True)
+    # The amount of ratings the game has received
+    rating_count = models.IntegerField(null=True, blank=True)
+    # The release dates of the game
+    release_dates = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The screenshots of the game
+    screenshots = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The similar games to the game
+    similar_games = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The slug of the game
+    slug = models.SlugField(max_length=255, null=True, blank=True)
+    # The standalone expansions of the game
+    standalone_expansions = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The status of the game
+    status = models.IntegerField(null=True, blank=True)
+    # The storylines of the game
+    storylines = models.TextField(null=True, blank=True)
+    # The summary of the game
+    summary = models.TextField(null=True, blank=True)
+    # The tags of the game
+    tags = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The themes of the game
+    themes = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The total rating of the game
+    total_rating = models.FloatField(null=True, blank=True)
+    # The amount of total ratings the game has received
+    total_rating_count = models.IntegerField(null=True, blank=True)
+    # The update date of the game
+    updated_at = models.IntegerField(null=True, blank=True)
+    # The URL of the game
+    url = models.URLField(max_length=255, null=True, blank=True)
+    # The version parent of the game
+    version_parent = models.IntegerField(null=True, blank=True)
+    # The version title of the game
+    version_title = models.CharField(max_length=255, null=True, blank=True)
+    # The videos associated with the game
+    videos = ArrayField(base_field=models.IntegerField, null=True, blank=True)
+    # The websites of the game
+    websites = ArrayField(base_field=models.IntegerField, null=True, blank=True)
 
     def __str__(self):
         return self.to_json().__str__()
