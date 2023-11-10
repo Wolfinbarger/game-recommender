f3b97e8a82e5918ded38dc8787b004453f4294a7
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-11-10 13:41:56 -0600

    re-added components

--- src/app/src/App.jsx
+++ src/app/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./global.scss";
+import styles from "./home.module.scss";
+import Footer from "./components/footer/Footer";
+import Nav from "./components/nav/Nav";
+import Cards from "./components/cards/Cards";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Nav />
+      <Cards />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/app/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.scss";
+const Footer = () => {
+  return (
+    <footer className="flex flex-row justify-between px-5">
+      <p className="text-xs">Copyright @ 2023 Clever Penguin Design</p>
+      <p className="text-xs ">Contact Us</p>
+    </footer>
+  );
+};
+
+export default Footer;
