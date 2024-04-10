89bbcc91d211ed895ffc4ec53871e748e33dd3b3
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2024-04-09 21:35:34 -0500

    Created passing test

--- src/app/src/__test__/cards.test.jsx
+++ src/app/src/__test__/cards.test.jsx
@@ -1,23 +1,15 @@
-import jest from "jest";
-jest.mock("./card.scss", () => {
-  return {};
-});
+import fetchData from "../components/cards/Cards";
 
-import {fetchData} from "../components/cards/Cards";
-
-jest.mock("../__mocks__/Cards.jsx");
+jest.mock("../components/cards/Cards.jsx");
 
 describe("cards", () => {
   it("gets the the game(s) information", async () => {
     expect.hasAssertions();
     //Arrange
-
-
     //Act
     const data = await fetchData();
 
     //Assert
-
     expect(data.num_of_objects).toBe(0);
   });
 });
